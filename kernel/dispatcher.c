1784027a62a5867b8688e0dec0ee080c45e6ee21
iagoBc <iagobariuka@gmail.com>
2026-08-17 15:38:12 -0300

    refatoração do dispatcher: reorganização do código e inclusão de headers

--- kernel/dispatcher.c
+++ kernel/dispatcher.c
@@ -4,15 +4,24 @@
 
 // Dispatcher: gerencia os estados das tarefas.
 
-void dispatcher_init()
-{
+#include "task.h"
+#include "macros.h"
+extern void user_main(void *arg);
+
+
+void dispatcher_init(){
+    ppos_debug("subsystem dispatcher initiated\n");
 }
 
-void dispatcher_term()
-{
+void dispatcher_term(){
+    ppos_debug("dispatcher stopping, no more user tasks\n");
 }
 
-void dispatcher()
-{
+void dispatcher(){
+    ppos_debug("dispatcher started\n");
+    struct task_t *task_user = task_create("user_main", user_main, NULL);
+    task_switch(task_user);
+    task_destroy(task_user);
+    
 }
 
