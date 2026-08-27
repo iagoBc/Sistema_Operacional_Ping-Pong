bac87c68c3ddfbea752dcc3aa2efb27418f2d769
iagoBc <iagobariuka@gmail.com>
2026-08-27 18:56:51 -0300

    implementadas as funcoes de setar e retornar prioridade das tarefas, por ultimo implementado escalonador de tarefas por prioridade de menor valor

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -3,19 +3,45 @@
 // Este arquivo PODE/DEVE ser alterado.
 
 // Escalonador de tarefas prontas.
+#include "scheduler.h"
+#include "tcb.h"
 
-#define NULL 0
-#include "lib/queue.h"
+extern struct task_t *current_task;
 
-void sched_init()
-{
+// muda a prioridade de uma tarefa (da tarefa atual se task == NULL)
+void sched_setprio(struct task_t *task, int prio){
+    if(prio < -20 || prio > 20) return;
+    else if(task == NULL) task = current_task;
+    task->static_prio = task->dynamic_prio = prio;
 }
 
-void sched_term()
-{
+// obtém a prioridade de uma tarefa (da tarefa atual se task == NULL)
+int sched_getprio(struct task_t *task){
+    if(!task) return current_task->static_prio;
+    else return task->static_prio;
 }
 
-// função escalonador FCFS: devolve a próxima tarefa a escalonar na fila
-struct task_t *scheduler(struct queue_t *ready_queue){
-    return queue_head(ready_queue);
+void sched_init(){}
+
+void sched_term(){}
+
+// função escalonador: devolve a próxima tarefa a escalonar na fila
+struct task_t* scheduler(struct queue_t* ready_queue) {
+    struct task_t *next = queue_head(ready_queue);
+    struct task_t *it = queue_next(ready_queue);
+
+    while(it){ // percorre a fila de tarefas prontas para encontrar a de maior prioridade(menor valor de prioridade)
+        if (it->dynamic_prio < next->dynamic_prio) next = it;
+        it = queue_next(ready_queue);
+    }
+
+    it = queue_head(ready_queue); 
+
+    while(it){ // percorre a fila de tarefas prontas para envlehecer a prioridade dinâmica das demais tarefas, exceto a escolhida para execução
+        if (it != next && it->dynamic_prio > -20) it->dynamic_prio--;
+        it = queue_next(ready_queue);
+    }
+
+    next->dynamic_prio = next->static_prio; // restaura prioridade da tarefa escolhida para execução
+    return next;
 }
